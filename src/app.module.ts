a17897079edda418493aa8194b640b134bd809f7
nicoContigliani <nico.contigliani>
2024-08-29 18:34:53 -0300

    crud products

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
 import typeorm from './config/typeorm';
 
 @Module({
@@ -15,6 +16,7 @@ import typeorm from './config/typeorm';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
     }),
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
